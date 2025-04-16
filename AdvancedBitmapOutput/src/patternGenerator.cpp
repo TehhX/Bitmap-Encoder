45e4a140ed21c7497bd9ca64efb02015bc34949d
Sam <156055395+TehhX@users.noreply.github.com>
2025-04-16 03:21:46 -0400

    Formula generator now uses passed pixel.

--- AdvancedBitmapOutput/src/patternGenerator.cpp
+++ AdvancedBitmapOutput/src/patternGenerator.cpp
@@ -14,5 +14,5 @@ Image& PatternGen::getImage() {
 Formula::Formula(size_t w, size_t h, size_t (*formula)(size_t), const Pixel& pix) 
 : PatternGen { w, h } {
     for (size_t x { 0 }; x < img.w; x++)
-        img.getPixel(x, std::max(std::min(formula(x), h - 1), 0ULL)) = { 255, 255, 255 };
+        img.getPixel(x, std::max(std::min(formula(x), h - 1), 0ULL)) = pix;
 }
